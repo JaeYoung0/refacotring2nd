d0574b04543197cfc16ebaa914c3a014fc37440a
JaeYoungHeo <hjy8697@gmail.com>
2021-02-11 10:51:29 +0900

    ch1 - 함수 추출하기

--- src/pages/chapter01/Chapter01.tsx
+++ src/pages/chapter01/Chapter01.tsx
@@ -32,35 +32,45 @@ const Chapter01 = () => {
     othello: { name: 'Othello', type: 'tragedy' },
   }
 
+  // 값이 바뀌지 않는 변수는 매개변수로 전달
+  const amountFor = (perf: any, play: any) => {
+    // 변수 초기화
+    let thisAmount = 0
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30)
+        }
+        break
+
+      case 'comedy':
+        thisAmount = 30000
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20)
+        }
+        thisAmount += 300 * perf.audience
+        break
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`)
+    }
+
+    // 함수 안에서 값이 바뀌는 변수 반환
+    return thisAmount
+  }
+
   const statement = (invoice: any, plays: any) => {
     let totalAmount = 0
     let volumeCredits = 0
-    let results = `청구 내역 (고객명: ${invoice.customer}\n`
-    const format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 })
+    let results = `청구 내역 (고객명: ${invoice.customer})\n`
+    const format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'KRW', maximumFractionDigits: 2 })
       .format
 
     invoice.performances.map((perf: any) => {
       const play = plays[perf.playID]
-      let thisAmount = 0
 
-      switch (play.type) {
-        case 'tragedy':
-          thisAmount = 40000
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30)
-          }
-          break
-
-        case 'comedy':
-          thisAmount = 30000
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20)
-          }
-          thisAmount += 300 * perf.audience
-          break
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`)
-      }
+      // 위에서 추출한 함수를 이용
+      let thisAmount = amountFor(perf, play)
 
       //   포인트를 적립한다.
       volumeCredits += Math.max(perf.audience - 30, 0)
@@ -69,18 +79,24 @@ const Chapter01 = () => {
       if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
 
       // 청구 내역을 출력한다.
-      results += `총액: ${format(totalAmount / 100)}\n`
-      results += `적립 포인트: ${volumeCredits}점\n`
-      console.log(results)
-      return results
+      results += `${play.name}: ${format(thisAmount / 100)} (${perf.audience})석 \n`
+      totalAmount += thisAmount
     })
+    results += `총액: ${format(totalAmount / 100)}\n`
+    results += `적립 포인트: ${volumeCredits}점\n`
+    console.log(results)
+    return (
+      <div>
+        <span>{results}</span>
+      </div>
+    )
   }
 
   return (
-    <div>
-      This is Chapter01...
+    <>
+      <h1>Chapter01... </h1>
       {statement(invoice, plays)}
-    </div>
+    </>
   )
 }
 
