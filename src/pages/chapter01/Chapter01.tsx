104527c12fbd75ecf58d7e554eceae8e00522ee5
JaeYoungHeo <hjy8697@gmail.com>
2021-02-11 12:17:17 +0900

    ch01: 중첩함수

--- src/pages/chapter01/Chapter01.tsx
+++ src/pages/chapter01/Chapter01.tsx
@@ -13,67 +13,67 @@ interface play {
 }
 
 const Chapter01 = () => {
-  const totalVolumeCredits = () => {
-    let volumeCredits = 0
-    invoice.performances.map((perf: aPerformance) => {
-      volumeCredits += volumeCreditsFor(perf)
-    })
-
-    return volumeCredits
-  }
-
-  const usd = (aNumber: number) => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(
-      aNumber / 100
-    )
-  }
-
-  const volumeCreditsFor = (aPerformance: aPerformance) => {
-    //   포인트를 적립한다.
-    let volumeCredits = 0
-    volumeCredits += Math.max(aPerformance.audience - 30, 0)
-
-    // 희극관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5)
-
-    return volumeCredits
-  }
-
-  const playFor = (aPerformance: aPerformance) => plays[aPerformance.playID]
-
-  const amountFor = (aPerformance: any) => {
-    let thisAmount = 0
-    switch (playFor(aPerformance).type) {
-      case 'tragedy':
-        thisAmount = 40000
-        if (aPerformance.audience > 30) {
-          thisAmount += 1000 * (aPerformance.audience - 30)
-        }
-        break
-
-      case 'comedy':
-        thisAmount = 30000
-        if (aPerformance.audience > 20) {
-          thisAmount += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        thisAmount += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+  const statement = (invoice: any, plays: any) => {
+    const totalAmount = () => {
+      let result = 0
+      invoice.performances.map((perf: aPerformance) => {
+        result += amountFor(perf)
+      })
+      return result
     }
 
-    return thisAmount
-  }
+    const totalVolumeCredits = () => {
+      let volumeCredits = 0
+      invoice.performances.map((perf: aPerformance) => {
+        volumeCredits += volumeCreditsFor(perf)
+      })
 
-  const totalAmount = () => {
-    let result = 0
-    invoice.performances.map((perf: aPerformance) => {
-      result += amountFor(perf)
-    })
-    return result
-  }
+      return volumeCredits
+    }
 
-  const statement = (invoice: any, plays: any) => {
+    const usd = (aNumber: number) => {
+      return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(
+        aNumber / 100
+      )
+    }
+
+    const volumeCreditsFor = (aPerformance: aPerformance) => {
+      //   포인트를 적립한다.
+      let volumeCredits = 0
+      volumeCredits += Math.max(aPerformance.audience - 30, 0)
+
+      // 희극관객 5명마다 추가 포인트를 제공한다.
+      if ('comedy' === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5)
+
+      return volumeCredits
+    }
+
+    const playFor = (aPerformance: aPerformance) => plays[aPerformance.playID]
+
+    const amountFor = (aPerformance: any) => {
+      let thisAmount = 0
+      switch (playFor(aPerformance).type) {
+        case 'tragedy':
+          thisAmount = 40000
+          if (aPerformance.audience > 30) {
+            thisAmount += 1000 * (aPerformance.audience - 30)
+          }
+          break
+
+        case 'comedy':
+          thisAmount = 30000
+          if (aPerformance.audience > 20) {
+            thisAmount += 10000 + 500 * (aPerformance.audience - 20)
+          }
+          thisAmount += 300 * aPerformance.audience
+          break
+        default:
+          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+      }
+
+      return thisAmount
+    }
+
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
     invoice.performances.map((perf: aPerformance) => {
@@ -83,11 +83,7 @@ const Chapter01 = () => {
     result += `총액: ${usd(totalAmount())}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
     console.log(result)
-    return (
-      <div>
-        <span>{result}</span>
-      </div>
-    )
+    return result
   }
 
   return (
