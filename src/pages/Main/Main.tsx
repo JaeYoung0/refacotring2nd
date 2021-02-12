f672453aba2741b641616bc0dc42c41119eb55be
JaeYoungHeo <hjy8697@gmail.com>
2021-02-12 19:57:31 +0900

    material UI

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
 import router from 'next/router'
+import Button from '@material-ui/core/Button'
 
 const Main = () => {
   return (
     <div>
-      <button
+      <Button
+        variant="outlined"
+        color="primary"
         onClick={() => {
           router.push('/chapter01')
         }}
       >
         chapter 01
-      </button>
+      </Button>
     </div>
   )
 }
